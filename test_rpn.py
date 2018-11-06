2aa364ebbff05a4e7b7c4df3d6e3546d42dffdcf
tchon <tchon@umich.edu>
2018-11-05 21:09:06 -0500

    Added exponential test

--- test_rpn.py
+++ test_rpn.py
@@ -15,3 +15,8 @@ class TestBasics(unittest.TestCase):
     def test_divide(self):
         result = rpn.calculate("6 3 /")
         self.assertEqual(2, result)
+    def test_exponent(self):
+        result = rpn.calculate("6 2 ^")
+        self.assertEqual(36, result)
+
+
